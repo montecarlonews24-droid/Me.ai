89f926bb512f12ad337dd773e2acc98636da7db2
montecarlonews24-droid <montecarlo.news24@gmail.com>
2026-07-23 20:19:27 +0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+// Kill-switch service worker: removes itself and clears old caches
+// so users always get the latest deployed version.
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((k) => caches.delete(k)));
+      await self.registration.unregister();
+      const clientsList = await self.clients.matchAll({ type: 'window' });
+      clientsList.forEach((client) => client.navigate(client.url));
+    })()
+  );
+});
